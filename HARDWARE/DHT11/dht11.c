c0f99e6fc2996fa58c28e609a3bc14cdf6216b05
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-02 10:00:41 +0800

    Update dht11.c

--- HARDWARE/DHT11/dht11.c
+++ HARDWARE/DHT11/dht11.c
@@ -85,6 +85,49 @@ u8 DHT11_Read_Byte(void)
 
 
 
+//从DHT11读取一次数据
+//temp:温度值(范围:0~50°)
+//humi:湿度值(范围:20%~90%)
+//返回值：0,正常;1,读取失败
+u8 DHT11_Read_Data(u8 *temp,u8 *humi)    
+{        
+ 	u8 buf[5];
+	u8 i;
+	DHT11_Rst();
+	if(DHT11_Check()==0)
+	{
+		for(i=0;i<5;i++)//读取40位数据
+		{
+			buf[i]=DHT11_Read_Byte();
+		}
+		if((buf[0]+buf[1]+buf[2]+buf[3])==buf[4])
+		{
+			*humi=buf[0];
+			*temp=buf[2];
+		}
+	}else return 1;
+	return 0;	    
+}
+//初始化DHT11的IO口 DQ 同时检测DHT11的存在
+//返回1:不存在
+//返回0:存在    	 
+u8 DHT11_Init(void)
+{
+	RCC->AHB1ENR|=1<<6;    //使能PORTG时钟	   	  
+	GPIO_Set(GPIOG,PIN9,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_50M,GPIO_PUPD_PU);//PG9设置  
+	DHT11_Rst();
+	return DHT11_Check();
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
