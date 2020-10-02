6a2e7e44970f4fd256c752107ff5bb76fe97a7fb
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-02 09:57:53 +0800

    Update dht11.c

--- HARDWARE/DHT11/dht11.c
+++ HARDWARE/DHT11/dht11.c
@@ -17,7 +17,28 @@ void DHT11_Rst(void)
     DHT11_DQ_OUT=1; 	//DQ=1 
 	delay_us(30);     	//主机拉高20~40us
 }
-
+//等待DHT11的回应
+//返回1:未检测到DHT11的存在
+//返回0:存在
+u8 DHT11_Check(void) 	   
+{   
+	u8 retry=0;
+	DHT11_IO_IN();//SET INPUT	 
+    while (DHT11_DQ_IN&&retry<100)//DHT11会拉低40~80us
+	{
+		retry++;
+		delay_us(1);
+	};	 
+	if(retry>=100)return 1;
+	else retry=0;
+    while (!DHT11_DQ_IN&&retry<100)//DHT11拉低后会再次拉高40~80us
+	{
+		retry++;
+		delay_us(1);
+	};
+	if(retry>=100)return 1;	    
+	return 0;
+}
 
 
 
