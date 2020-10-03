450b38fa842606491031ccdc54abed6a62cb7429
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-03 13:18:07 +0800

    Update ds18b20.c

--- HARDWARE/DS18B20/ds18b20.c
+++ HARDWARE/DS18B20/ds18b20.c
@@ -11,3 +11,37 @@ void DS18B20_Rst(void)
     DS18B20_DQ_OUT=1; //DQ=1 
 	delay_us(15);     //15US
 }
+//等待DS18B20的回应
+//返回1:未检测到DS18B20的存在
+//返回0:存在
+u8 DS18B20_Check(void) 	   
+{   
+	u8 retry=0;
+	DS18B20_IO_IN();//SET PG11 INPUT	 
+    while (DS18B20_DQ_IN&&retry<200)
+	{
+		retry++;
+		delay_us(1);
+	};	 
+	if(retry>=200)return 1;
+	else retry=0;
+    while (!DS18B20_DQ_IN&&retry<240)
+	{
+		retry++;
+		delay_us(1);
+	};
+	if(retry>=240)return 1;	    
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
