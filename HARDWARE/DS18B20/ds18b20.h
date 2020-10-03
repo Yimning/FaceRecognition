bf8540b9077681ef8ebc99dd88d0eec9d25d3034
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-03 13:21:05 +0800

    Update ds18b20.h

--- HARDWARE/DS18B20/ds18b20.h
+++ HARDWARE/DS18B20/ds18b20.h
@@ -2,19 +2,8 @@
 #define __DS18B20_H 
 #include "sys.h"   
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//IO方向设置
+#define DS18B20_IO_IN()  {GPIOG->MODER&=~(3<<(9*2));GPIOG->MODER|=0<<9*2;}	//PG9输入模式
+#define DS18B20_IO_OUT() {GPIOG->MODER&=~(3<<(9*2));GPIOG->MODER|=1<<9*2;} 	//PG9输出模式
 
 
