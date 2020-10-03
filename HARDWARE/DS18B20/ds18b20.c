867b235745ff2df9aed9ea07a017e6bbce3c65b5
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-03 13:19:40 +0800

    Update ds18b20.c

--- HARDWARE/DS18B20/ds18b20.c
+++ HARDWARE/DS18B20/ds18b20.c
@@ -113,6 +113,35 @@ u8 DS18B20_Init(void)
 }  
 
 
+//从ds18b20得到温度值
+//精度：0.1C
+//返回值：温度值 （-550~1250） 
+short DS18B20_Get_Temp(void)
+{
+    u8 temp;
+    u8 TL,TH;
+	short tem;
+    DS18B20_Start();// ds1820 start convert
+    DS18B20_Rst();
+    DS18B20_Check();	 
+    DS18B20_Write_Byte(0xcc);// skip rom
+    DS18B20_Write_Byte(0xbe);// convert	    
+    TL=DS18B20_Read_Byte(); // LSB   
+    TH=DS18B20_Read_Byte(); // MSB   
+    if(TH>7)
+    {
+        TH=~TH;
+        TL=~TL; 
+        temp=0;		//温度为负  
+    }else temp=1;	//温度为正	  	  
+    tem=TH; 		//获得高八位
+    tem<<=8;    
+    tem+=TL;		//获得底八位
+    tem=(double)tem*0.625;	//转换     
+	if(temp)return tem;	 	//返回温度值
+	else return -tem;    
+}
+
 
 
 
