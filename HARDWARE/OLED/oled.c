9cb91830fbb6f045a2468899ce8ff120f9c0969a
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-04 11:51:44 +0800

    Update oled.c

--- HARDWARE/OLED/oled.c
+++ HARDWARE/OLED/oled.c
@@ -12,13 +12,43 @@
 ////////////////////////////////////////////////////////////////////////////////// 	  
  
 
+//OLED的显存
+//存放格式如下.
+//[0]0 1 2 3 ... 127	
+//[1]0 1 2 3 ... 127	
+//[2]0 1 2 3 ... 127	
+//[3]0 1 2 3 ... 127	
+//[4]0 1 2 3 ... 127	
+//[5]0 1 2 3 ... 127	
+//[6]0 1 2 3 ... 127	
+//[7]0 1 2 3 ... 127 		   
+u8 OLED_GRAM[128][8];	 
 
-
-
-
-
-
-
+//更新显存到LCD		 
+void OLED_Refresh_Gram(void)
+{
+	u8 i,n;		    
+	for(i=0;i<8;i++)  
+	{  
+		OLED_WR_Byte (0xb0+i,OLED_CMD);    //设置页地址（0~7）
+		OLED_WR_Byte (0x00,OLED_CMD);      //设置显示位置—列低地址
+		OLED_WR_Byte (0x10,OLED_CMD);      //设置显示位置—列高地址   
+		for(n=0;n<128;n++)OLED_WR_Byte(OLED_GRAM[n][i],OLED_DATA); 
+	}   
+}
+#if OLED_MODE==1	//8080并口
+//通过拼凑的方法向OLED输出一个8位数据
+//data:要输出的数据
+void OLED_Data_Out(u8 data)
+{
+	u16 dat=data&0X0F;
+	GPIOC->ODR&=~(0XF<<6);		//清空6~9
+	GPIOC->ODR|=dat<<6;			//D[3:0]-->PC[9:6]
+	PCout(11)=(data>>4)&0X01;	//D4
+	PBout(6)=(data>>5)&0X01;	//D5
+	PEout(5)=(data>>6)&0X01;	//D6
+	PEout(6)=(data>>7)&0X01;	//D7 
+} 
 
 
 
