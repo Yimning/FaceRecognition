046c2e95716793db7c541e6b948cd25d204010eb
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-04 11:50:19 +0800

    Update oled.h

--- HARDWARE/OLED/oled.h
+++ HARDWARE/OLED/oled.h
@@ -32,4 +32,17 @@
 #define OLED_CMD  	0		//写命令
 #define OLED_DATA 	1		//写数据
 
-	 
+//OLED控制用函数
+void OLED_WR_Byte(u8 dat,u8 cmd);	    
+void OLED_Display_On(void);
+void OLED_Display_Off(void);
+void OLED_Refresh_Gram(void);		   
+							   		    
+void OLED_Init(void);
+void OLED_Clear(void);
+void OLED_DrawPoint(u8 x,u8 y,u8 t);
+void OLED_Fill(u8 x1,u8 y1,u8 x2,u8 y2,u8 dot);
+void OLED_ShowChar(u8 x,u8 y,u8 chr,u8 size,u8 mode);
+void OLED_ShowNum(u8 x,u8 y,u32 num,u8 len,u8 size);
+void OLED_ShowString(u8 x,u8 y,const u8 *p,u8 size);	 
+#endif  	 
