a41fb4cffd364bf02a660e420d5da1bbb94588b3
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-04 11:53:55 +0800

    Update oled.c

--- HARDWARE/OLED/oled.c
+++ HARDWARE/OLED/oled.c
@@ -213,7 +213,65 @@ void OLED_ShowString(u8 x,u8 y,const u8 *p,u8 size)
     }  
 	
 }	
+//初始化SSD1306					    
+void OLED_Init(void)
+{ 	 		 
+	RCC->AHB1ENR|=0X5F; //使能PORTA~E,PORTG时钟  
+#if OLED_MODE==1		//使用8080并口模式		  
+	GPIO_Set(GPIOA,PIN4,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU);		 	//PA4设置
+	GPIO_Set(GPIOB,PIN6|PIN7,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU); 	//PB6,7设置
+	GPIO_Set(GPIOC,PIN6|PIN7|PIN8|PIN9|PIN11,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU); //PC6~9,PC11设置
+	GPIO_Set(GPIOD,PIN6|PIN7,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU); 	//PD6,7设置
+	GPIO_Set(GPIOE,PIN5|PIN6,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU); 	//PE5,6设置
+	GPIO_Set(GPIOG,PIN15,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU); 		//PG15设置
+ 	OLED_WR=1;
+	OLED_RD=1; 
+#else					//使用4线SPI 串口模式
+	GPIO_Set(GPIOB,PIN7,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU); 		//PB7设置
+	GPIO_Set(GPIOC,PIN6|PIN7,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU); //PC6,7设置
+	GPIO_Set(GPIOD,PIN6,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU); 		//PD6设置
+	GPIO_Set(GPIOG,PIN15,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU); 	//PG15设置
+	OLED_SDIN=1;
+	OLED_SCLK=1;
+#endif
+	OLED_CS=1;
+	OLED_RS=1;	 
+	
+	OLED_RST=0;
+	delay_ms(100);
+	OLED_RST=1; 
+					  
+	OLED_WR_Byte(0xAE,OLED_CMD); //关闭显示
+	OLED_WR_Byte(0xD5,OLED_CMD); //设置时钟分频因子,震荡频率
+	OLED_WR_Byte(80,OLED_CMD);   //[3:0],分频因子;[7:4],震荡频率
+	OLED_WR_Byte(0xA8,OLED_CMD); //设置驱动路数
+	OLED_WR_Byte(0X3F,OLED_CMD); //默认0X3F(1/64) 
+	OLED_WR_Byte(0xD3,OLED_CMD); //设置显示偏移
+	OLED_WR_Byte(0X00,OLED_CMD); //默认为0
 
+	OLED_WR_Byte(0x40,OLED_CMD); //设置显示开始行 [5:0],行数.
+													    
+	OLED_WR_Byte(0x8D,OLED_CMD); //电荷泵设置
+	OLED_WR_Byte(0x14,OLED_CMD); //bit2，开启/关闭
+	OLED_WR_Byte(0x20,OLED_CMD); //设置内存地址模式
+	OLED_WR_Byte(0x02,OLED_CMD); //[1:0],00，列地址模式;01，行地址模式;10,页地址模式;默认10;
+	OLED_WR_Byte(0xA1,OLED_CMD); //段重定义设置,bit0:0,0->0;1,0->127;
+	OLED_WR_Byte(0xC0,OLED_CMD); //设置COM扫描方向;bit3:0,普通模式;1,重定义模式 COM[N-1]->COM0;N:驱动路数
+	OLED_WR_Byte(0xDA,OLED_CMD); //设置COM硬件引脚配置
+	OLED_WR_Byte(0x12,OLED_CMD); //[5:4]配置
+		 
+	OLED_WR_Byte(0x81,OLED_CMD); //对比度设置
+	OLED_WR_Byte(0xEF,OLED_CMD); //1~255;默认0X7F (亮度设置,越大越亮)
+	OLED_WR_Byte(0xD9,OLED_CMD); //设置预充电周期
+	OLED_WR_Byte(0xf1,OLED_CMD); //[3:0],PHASE 1;[7:4],PHASE 2;
+	OLED_WR_Byte(0xDB,OLED_CMD); //设置VCOMH 电压倍率
+	OLED_WR_Byte(0x30,OLED_CMD); //[6:4] 000,0.65*vcc;001,0.77*vcc;011,0.83*vcc;
+
+	OLED_WR_Byte(0xA4,OLED_CMD); //全局显示开启;bit0:1,开启;0,关闭;(白屏/黑屏)
+	OLED_WR_Byte(0xA6,OLED_CMD); //设置显示方式;bit0:1,反相显示;0,正常显示	    						   
+	OLED_WR_Byte(0xAF,OLED_CMD); //开启显示	 
+	OLED_Clear();
+}  
 
 
 
