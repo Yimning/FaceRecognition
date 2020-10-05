92704982552e8bff64f3f38a53972439d68b7502
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-05 18:01:24 +0800

    Update 24cxx.c

---
+++ HARDWARE/24CXX/24cxx.c
@@ -0,0 +1,51 @@
+#include "24cxx.h" 
+#include "delay.h" 				 
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//ALIENTEK STM32F407开发板
+//24CXX 驱动代码	   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//创建日期:2014/5/6
+//版本：V1.0
+//版权所有，盗版必究。
+//Copyright(C) 广州市星翼电子科技有限公司 2014-2024
+//All rights reserved									  
+////////////////////////////////////////////////////////////////////////////////// 	
+
+//初始化IIC接口
+void AT24CXX_Init(void)
+{
+	IIC_Init();
+}
+//在AT24CXX指定地址读出一个数据
+//ReadAddr:开始读数的地址  
+//返回值  :读到的数据
+u8 AT24CXX_ReadOneByte(u16 ReadAddr)
+{				  
+	u8 temp=0;		  	    																 
+    IIC_Start();  
+	if(EE_TYPE>AT24C16)
+	{
+		IIC_Send_Byte(0XA0);	   //发送写命令
+		IIC_Wait_Ack();
+		IIC_Send_Byte(ReadAddr>>8);//发送高地址	    
+	}else IIC_Send_Byte(0XA0+((ReadAddr/256)<<1));   //发送器件地址0XA0,写数据 	   
+	IIC_Wait_Ack(); 
+    IIC_Send_Byte(ReadAddr%256);   //发送低地址
+	IIC_Wait_Ack();	    
+	IIC_Start();  	 	   
+	IIC_Send_Byte(0XA1);           //进入接收模式			   
+	IIC_Wait_Ack();	 
+    temp=IIC_Read_Byte(0);		   
+    IIC_Stop();//产生一个停止条件	    
+	return temp;
+}
+
+
+
+
+
+
+
+
