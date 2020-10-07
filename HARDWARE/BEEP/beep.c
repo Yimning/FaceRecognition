a5165d300eea800115aa0fad80d4880f12ca121c
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-07 15:17:57 +0800

    Update beep.c

--- HARDWARE/BEEP/beep.c
+++ HARDWARE/BEEP/beep.c
@@ -1,26 +1,11 @@
 #include "beep.h" 
 
-
-//初始化PF8为输出口		    
-//BEEP IO初始化
 void BEEP_Init(void)
 {    	 
 	RCC->AHB1ENR|=1<<5;    		//使能PORTF时钟 
 	GPIO_Set(GPIOF,PIN8,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PD); //PF8设置,下拉
-
+	BEEP=0;						//关闭蜂鸣器	
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
