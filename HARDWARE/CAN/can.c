f8445f13f4e91e785de37ef18b263bc1eba9efc3
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-08 13:57:31 +0800

    Update can.c

--- HARDWARE/CAN/can.c
+++ HARDWARE/CAN/can.c
@@ -21,4 +21,57 @@
 //返回值:0,初始化OK;
 //    其他,初始化失败;
 
+u8 CAN1_Mode_Init(u8 tsjw,u8 tbs2,u8 tbs1,u16 brp,u8 mode)
+{
+	u16 i=0;
+ 	if(tsjw==0||tbs2==0||tbs1==0||brp==0)return 1;
+	tsjw-=1;//先减去1.再用于设置
+	tbs2-=1;
+	tbs1-=1;
+	brp-=1;
+
+	RCC->AHB1ENR|=1<<0;  	//使能PORTA口时钟 
+	GPIO_Set(GPIOA,PIN11|PIN12,GPIO_MODE_AF,GPIO_OTYPE_PP,GPIO_SPEED_50M,GPIO_PUPD_PU);//PA11,PA12,复用功能,上拉输出
+ 	GPIO_AF_Set(GPIOA,11,9);//PA11,AF9
+	GPIO_AF_Set(GPIOA,12,9);//PA12,AF9 	   
+ 
+	RCC->APB1ENR|=1<<25;//使能CAN1时钟 CAN1使用的是APB1的时钟(max:42M)
+	CAN1->MCR=0x0000;	//退出睡眠模式(同时设置所有位为0)
+	CAN1->MCR|=1<<0;		//请求CAN进入初始化模式
+	while((CAN1->MSR&1<<0)==0)
+	{
+		i++;
+		if(i>100)return 2;//进入初始化模式失败
+	}
+	CAN1->MCR|=0<<7;		//非时间触发通信模式
+	CAN1->MCR|=0<<6;		//软件自动离线管理
+	CAN1->MCR|=0<<5;		//睡眠模式通过软件唤醒(清除CAN1->MCR的SLEEP位)
+	CAN1->MCR|=1<<4;		//禁止报文自动传送
+	CAN1->MCR|=0<<3;		//报文不锁定,新的覆盖旧的
+	CAN1->MCR|=0<<2;		//优先级由报文标识符决定
+	CAN1->BTR=0x00000000;	//清除原来的设置.
+	CAN1->BTR|=mode<<30;	//模式设置 0,普通模式;1,回环模式;
+	CAN1->BTR|=tsjw<<24; 	//重新同步跳跃宽度(Tsjw)为tsjw+1个时间单位
+	CAN1->BTR|=tbs2<<20; 	//Tbs2=tbs2+1个时间单位
+	CAN1->BTR|=tbs1<<16;	//Tbs1=tbs1+1个时间单位
+	CAN1->BTR|=brp<<0;  	//分频系数(Fdiv)为brp+1
+							//波特率:Fpclk1/((Tbs1+Tbs2+1)*Fdiv)
+	CAN1->MCR&=~(1<<0);		//请求CAN退出初始化模式
+	while((CAN1->MSR&1<<0)==1)
+	{
+		i++;
+		if(i>0XFFF0)return 3;//退出初始化模式失败
+	}
+	//过滤器初始化
+	CAN1->FMR|=1<<0;		//过滤器组工作在初始化模式
+	CAN1->FA1R&=~(1<<0);	//过滤器0不激活
+	CAN1->FS1R|=1<<0; 		//过滤器位宽为32位.
+	CAN1->FM1R|=0<<0;		//过滤器0工作在标识符屏蔽位模式
+	CAN1->FFA1R|=0<<0;		//过滤器0关联到FIFO0
+	CAN1->sFilterRegister[0].FR1=0X00000000;//32位ID
+	CAN1->sFilterRegister[0].FR2=0X00000000;//32位MASK
+	CAN1->FA1R|=1<<0;		//激活过滤器0
+	CAN1->FMR&=0<<0;		//过滤器组进入正常模式
+
+
 
