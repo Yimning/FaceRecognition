3be38e47c51cbad1b973826fa3344652d793bd7b
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-09 12:21:00 +0800

    Update dac.c

--- HARDWARE/DAC/dac.c
+++ HARDWARE/DAC/dac.c
@@ -5,6 +5,10 @@
 void Dac1_Init(void)
 {   	
 
+	RCC->APB1ENR|=1<<29;   	//使能DAC时钟	   
+	RCC->AHB1ENR|=1<<0;  	//使能PORTA时钟	  
+	GPIO_Set(GPIOA,PIN4,GPIO_MODE_AIN,0,0,GPIO_PUPD_PU);//PA4,模拟输入,下拉   
+
 }
 
 //设置通道1输出电压
