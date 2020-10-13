74378d07f4545de860199fe5db6334508c6a9735
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-13 09:25:12 +0800

    Update key.c

--- HARDWARE/KEY/key.c
+++ HARDWARE/KEY/key.c
@@ -31,7 +31,10 @@ u8 KEY_Scan(u8 mode)
 	static u8 key_up=1;//按键按松开标志
 	if(mode)key_up=1;  //支持连按		  
 	if(key_up&&(KEY0==0||KEY1==0||KEY2==0||WK_UP==1))
+	{
 
+	}else if(KEY0==1&&KEY1==1&&KEY2==1&&WK_UP==0)key_up=1; 	    
+ 	return 0;// 无按键按下
 }
 
 
