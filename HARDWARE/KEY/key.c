37abb8b74691515f91b2c4bb91908fb91dd4e0b0
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-13 09:22:24 +0800

    Update key.c

--- HARDWARE/KEY/key.c
+++ HARDWARE/KEY/key.c
@@ -7,6 +7,18 @@
 //技术论坛:www.openedv.com
 ////////////////////////////////////////////////////////////////////////////////// 	 
 
+//按键初始化函数
+void KEY_Init(void)
+{
+	RCC->AHB1ENR|=1<<0;     //使能PORTA时钟 
+	RCC->AHB1ENR|=1<<4;     //使能PORTE时钟
+	GPIO_Set(GPIOE,PIN2|PIN3|PIN4,GPIO_MODE_IN,0,0,GPIO_PUPD_PU);	//PE2~4设置上拉输入
+	GPIO_Set(GPIOA,PIN0,GPIO_MODE_IN,0,0,GPIO_PUPD_PD); 			//PA0设置为下拉输入 
+} 
+
+
+
+
 
 
 
