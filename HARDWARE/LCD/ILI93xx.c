4f35966cfb5858cc17e47ae2bb6630afe2aa4c3f
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-14 11:56:07 +0800

    Update ILI93xx.c

--- HARDWARE/LCD/ILI93xx.c
+++ HARDWARE/LCD/ILI93xx.c
@@ -2888,7 +2888,17 @@ void LCD_ShowxNum(u16 x,u16 y,u32 num,u8 len,u8 size,u8 mode)
 //*p:字符串起始地址		  
 void LCD_ShowString(u16 x,u16 y,u16 width,u16 height,u8 size,u8 *p)
 {         
-  
+  	u8 x0=x;
+	width+=x;
+	height+=y;
+    while((*p<='~')&&(*p>=' '))//判断是不是非法字符!
+    {       
+        if(x>=width){x=x0;y+=size;}
+        if(y>=height)break;//退出
+        LCD_ShowChar(x,y,*p,size,0);
+        x+=size/2;
+        p++;
+    }  
 }
 
 
