40c4e06501afc4fe71f26cc850b291242c18ecd4
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-17 12:23:16 +0800

    Update mpu6050.c

--- HARDWARE/MPU6050/mpu6050.c
+++ HARDWARE/MPU6050/mpu6050.c
@@ -9,3 +9,29 @@
 //技术论坛:www.openedv.com
 ////////////////////////////////////////////////////////////////////////////////// 	
 
+//初始化MPU6050
+//返回值:0,成功
+//    其他,错误代码
+u8 MPU_Init(void)
+{ 
+	u8 res;
+	IIC_Init();//初始化IIC总线
+	MPU_Write_Byte(MPU_PWR_MGMT1_REG,0X80);	//复位MPU6050
+    delay_ms(100);
+	MPU_Write_Byte(MPU_PWR_MGMT1_REG,0X00);	//唤醒MPU6050 
+	MPU_Set_Gyro_Fsr(3);					//陀螺仪传感器,±2000dps
+	MPU_Set_Accel_Fsr(0);					//加速度传感器,±2g
+	MPU_Set_Rate(50);						//设置采样率50Hz
+	MPU_Write_Byte(MPU_INT_EN_REG,0X00);	//关闭所有中断
+	MPU_Write_Byte(MPU_USER_CTRL_REG,0X00);	//I2C主模式关闭
+	MPU_Write_Byte(MPU_FIFO_EN_REG,0X00);	//关闭FIFO
+	MPU_Write_Byte(MPU_INTBP_CFG_REG,0X80);	//INT引脚低电平有效
+	res=MPU_Read_Byte(MPU_DEVICE_ID_REG);
+	if(res==MPU_ADDR)//器件ID正确
+	{
+		MPU_Write_Byte(MPU_PWR_MGMT1_REG,0X01);	//设置CLKSEL,PLL X轴为参考
+		MPU_Write_Byte(MPU_PWR_MGMT2_REG,0X00);	//加速度与陀螺仪都工作
+		MPU_Set_Rate(50);						//设置采样率为50Hz
+ 	}else return 1;
+	return 0;
+}
