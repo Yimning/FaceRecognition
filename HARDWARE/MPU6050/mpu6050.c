4be4c044dbd7706b4493a6acea89cd40aabfd5d0
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-17 12:25:48 +0800

    Update mpu6050.c

--- HARDWARE/MPU6050/mpu6050.c
+++ HARDWARE/MPU6050/mpu6050.c
@@ -155,6 +155,36 @@ u8 MPU_Write_Len(u8 addr,u8 reg,u8 len,u8 *buf)
     IIC_Stop();	 
 	return 0;	
 } 
+//IIC连续读
+//addr:器件地址
+//reg:要读取的寄存器地址
+//len:要读取的长度
+//buf:读取到的数据存储区
+//返回值:0,正常
+//    其他,错误代码
+u8 MPU_Read_Len(u8 addr,u8 reg,u8 len,u8 *buf)
+{ 
+ 	IIC_Start(); 
+	IIC_Send_Byte((addr<<1)|0);//发送器件地址+写命令	
+	if(IIC_Wait_Ack())	//等待应答
+	{
+		IIC_Stop();		 
+		return 1;		
+	}
+    IIC_Send_Byte(reg);	//写寄存器地址
+    IIC_Wait_Ack();		//等待应答
+    IIC_Start();
+	IIC_Send_Byte((addr<<1)|1);//发送器件地址+读命令	
+    IIC_Wait_Ack();		//等待应答 
+	while(len)
+	{
+		if(len==1)*buf=IIC_Read_Byte(0);//读数据,发送nACK 
+		else *buf=IIC_Read_Byte(1);		//读数据,发送ACK  
+		len--;
+		buf++; 
+	}    
+    IIC_Stop();	//产生一个停止条件 
+	return 0;	
 
 
 
