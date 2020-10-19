af7348cc3d926917757bd573adb73f4c8f00bec4
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-19 15:30:00 +0800

    Update ov2640.c

---
+++ HARDWARE/OV2640/ov2640.c
@@ -0,0 +1,16 @@
+#include "sys.h"
+#include "ov2640.h"
+#include "ov2640cfg.h"
+#include "timer.h"	  
+#include "delay.h"
+#include "usart.h"			 
+#include "sccb.h"	
+#include "exti.h"  
+//////////////////////////////////////////////////////////////////////////////////	 
+//ALIENTEK STM32F407开发板
+//OV2640 驱动代码	   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//All rights reserved									  
+////////////////////////////////////////////////////////////////////////////////// 
+ 
