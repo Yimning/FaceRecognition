761cb18b7efa6a54a9e8ecb2986c2bd3b0e1e4ea
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-21 23:39:16 +0800

    Update rng.c

--- HARDWARE/RNG/rng.c
+++ HARDWARE/RNG/rng.c
@@ -11,8 +11,19 @@
 
 //初始化RNG
 //返回值:0,成功;1,失败
-
-
+u8 RNG_Init(void)
+{
+	u16 retry=0; 
+	RCC->AHB2ENR=1<<6;	//开启RNG时钟,来自PLL48CLK
+	RNG->CR|=1<<2;		//使能RNG
+	while((RNG->SR&0X01)==0&&retry<10000)	//等待随机数就绪
+	{
+		retry++;
+		delay_us(100);
+	}
+	if(retry>=10000)return 1;//随机数产生器工作不正常
+	return 0;
+}
 
 
 
