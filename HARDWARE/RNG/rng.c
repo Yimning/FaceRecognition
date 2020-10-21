53dd0823612f13d3b878cc45716d181fa15f4fd5
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-21 23:36:47 +0800

    Update rng.c

---
+++ HARDWARE/RNG/rng.c
@@ -0,0 +1,13 @@
+#include "rng.h"
+#include "delay.h"
+//ALIENTEK STM32F407开发板
+//RNG(随机数发生器)驱动代码	   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//All rights reserved									  
+////////////////////////////////////////////////////////////////////////////////// 
+
+
+
+
+
