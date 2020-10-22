7a275bd74fa5c75c8f95905bb0847173a4ac37ae
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-22 13:39:00 +0800

    Update rs485.c

--- HARDWARE/RS485/rs485.c
+++ HARDWARE/RS485/rs485.c
@@ -28,5 +28,42 @@ void USART2_IRQHandler(void)
 	}  											 
 } 
 #endif										 
+//初始化IO 串口2
+//pclk1:PCLK1时钟频率(Mhz),APB1一般为42Mhz
+//bound:波特率	  
+void RS485_Init(u32 pclk1,u32 bound)
+{  	 
+	float temp;
+	u16 mantissa;
+	u16 fraction;	   
+	temp=(float)(pclk1*1000000)/(bound*16);//得到USARTDIV
+	mantissa=temp;				 //得到整数部分
+	fraction=(temp-mantissa)*16; //得到小数部分	 
+    mantissa<<=4;
+	mantissa+=fraction; 
+	
+	RCC->AHB1ENR|=1<<0;   		//使能PORTA口时钟  
+	RCC->AHB1ENR|=1<<6;   		//使能PORTG口时钟  
+	GPIO_Set(GPIOG,PIN8,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU); 		//PG8推挽输出
+	GPIO_Set(GPIOA,PIN2|PIN3,GPIO_MODE_AF,GPIO_OTYPE_PP,GPIO_SPEED_50M,GPIO_PUPD_PU);	//PA2,PA3,复用功能,上拉 
+ 	GPIO_AF_Set(GPIOA,2,7);		//PA2,AF7
+	GPIO_AF_Set(GPIOA,3,7);		//PA3,AF7  	   
+ 
+	RCC->APB1ENR|=1<<17;  		//使能串口2时钟  
+	RCC->APB1RSTR|=1<<17;   	//复位串口2
+	RCC->APB1RSTR&=~(1<<17);	//停止复位	   	   
+	//波特率设置
+ 	USART2->BRR=mantissa; 		// 波特率设置	 
+	USART2->CR1|=0X200C;  		//1位停止,无校验位.
+#if EN_USART2_RX		  		//如果使能了接收
+	//使能接收中断 
+	USART2->CR1|=1<<2;  		//串口接收使能
+	USART2->CR1|=1<<5;    		//接收缓冲区非空中断使能	    	
+	MY_NVIC_Init(3,3,USART2_IRQn,2);//组2，最低优先级 
+#endif
+	RS485_TX_EN=0;				//默认为接收模式	
+}
+
+
 
 
