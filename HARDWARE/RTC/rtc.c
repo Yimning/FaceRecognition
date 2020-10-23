b0a0772f9ba9c35eb03b2d402d40debfa22260f7
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-23 22:04:39 +0800

    Update rtc.c

--- HARDWARE/RTC/rtc.c
+++ HARDWARE/RTC/rtc.c
@@ -31,7 +31,61 @@ u8 RTC_Wait_Synchro(void)
 	return 0;
 }
 
+//RTC进入初始化模式
+//返回值:0,成功;1,失败;
+u8 RTC_Init_Mode(void)
+{ 
+	u32 retry=0X10000; 
+	if(RTC->ISR&(1<<6))return 0; 
+	RTC->ISR|=1<<7;	//进入RTC初始化模式
+	while(retry&&((RTC->ISR&(1<<6))==0x00))//等待进入RTC初始化模式成功
+	{
+		retry--;
+	}
+    if(retry==0)return 1;	//同步失败
+	else return 0; 			//同步成功 
+}
+//RTC写入后备区域SRAM
+//BKPx:后备区寄存器编号,范围:0~19
+//data:要写入的数据,32位长度
+void RTC_Write_BKR(u32 BKRx,u32 data)
+{
+	u32 temp=0; 
+	temp=RTC_BASE+0x50+BKRx*4;   
+	(*(u32*)temp)=data; 
+}
+//RTC读取后备区域SRAM
+//BKPx:后备区寄存器编号,范围:0~19
+//返回值:读取到的数据
+u32 RTC_Read_BKR(u32 BKRx)
+{
+	u32 temp=0; 
+	temp=RTC_BASE+0x50+BKRx*4;  
+	return (*(u32*)temp);		//返回读取到的值
+}
 
+//十进制转换为BCD码
+//val:要转换的十进制数
+//返回值:BCD码
+u8 RTC_DEC2BCD(u8 val)
+{
+	u8 bcdhigh = 0; 
+	while(val>=10)
+	{
+		bcdhigh++;
+		val-=10;
+	} 
+	return ((u8)(bcdhigh<<4)|val);
+}
+//BCD码转换为十进制数据
+//val:要转换的BCD码
+//返回值:十进制数据
+u8 RTC_BCD2DEC(u8 val)
+{
+	u8 temp=0;
+	temp=(val>>4)*10;
+	return (temp+(val&0X0F));
+}
 
 
 
