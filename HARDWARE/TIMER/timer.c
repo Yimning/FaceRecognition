c6e16240b24f0c85b92eae28fd5fbfa0d4b1bedc
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-28 13:03:02 +0800

    Update timer.c

--- HARDWARE/TIMER/timer.c
+++ HARDWARE/TIMER/timer.c
@@ -8,3 +8,17 @@
 //技术论坛:www.openedv.com
 ////////////////////////////////////////////////////////////////////////////////// 	
 
+
+
+
+extern int reg_time;
+//定时器3中断服务程序	 
+void TIM3_IRQHandler(void)
+{ 		    		  			    
+	if(TIM3->SR&0X0001)//溢出中断
+	{
+		//printf("frame:%d\r\n",ov_frame);//打印帧率
+		reg_time++; 
+	}				   
+	TIM3->SR&=~(1<<0);//清除中断标志位 	    
+}
