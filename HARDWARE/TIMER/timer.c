b4ce22c948ce9c3ddcba023f5c10a19a948d0ef7
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-28 13:03:27 +0800

    Update timer.c

--- HARDWARE/TIMER/timer.c
+++ HARDWARE/TIMER/timer.c
@@ -22,3 +22,31 @@ void TIM3_IRQHandler(void)
 	}				   
 	TIM3->SR&=~(1<<0);//清除中断标志位 	    
 }
+//通用定时器3中断初始化
+//这里时钟选择为APB1的2倍，而APB1为42M
+//arr：自动重装值。
+//psc：时钟预分频数
+//定时器溢出时间计算方法:Tout=((arr+1)*(psc+1))/Ft us.
+//Ft=定时器工作频率,单位:Mhz
+//这里使用的是定时器3!
+void TIM3_Int_Init(u16 arr,u16 psc)
+{
+	RCC->APB1ENR|=1<<1;	//TIM3时钟使能    
+ 	TIM3->ARR=arr;  	//设定计数器自动重装值 
+	TIM3->PSC=psc;  	//预分频器	  
+	TIM3->DIER|=1<<0;   //允许更新中断	  
+	TIM3->CR1|=0x01;    //使能定时器3
+  	MY_NVIC_Init(1,3,TIM3_IRQn,2);	//抢占1，子优先级3，组2									 
+}
+
+
+
+
+
+
+
+
+
+
+
+
