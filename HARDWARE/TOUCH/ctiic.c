6cc1a1c834bd1455419a08dc09c7daef2be61981
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-29 16:50:20 +0800

    Update ctiic.c

--- HARDWARE/TOUCH/ctiic.c
+++ HARDWARE/TOUCH/ctiic.c
@@ -19,5 +19,61 @@ void CT_IIC_Init(void)
 	GPIO_Set(GPIOB,PIN0,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU); 	//PB0设置为推挽输出
 	GPIO_Set(GPIOF,PIN11,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU); //PF11设置为推挽输出
 }
+//产生IIC起始信号
+void CT_IIC_Start(void)
+{
+	CT_SDA_OUT();     //sda线输出
+	CT_IIC_SDA=1;	  	  
+	CT_IIC_SCL=1;
+	delay_us(30);
+ 	CT_IIC_SDA=0;//START:when CLK is high,DATA change form high to low 
+	CT_Delay();
+	CT_IIC_SCL=0;//钳住I2C总线，准备发送或接收数据 
+}	  
+//产生IIC停止信号
+void CT_IIC_Stop(void)
+{
+	CT_SDA_OUT();//sda线输出
+	CT_IIC_SCL=1;
+	delay_us(30);
+	CT_IIC_SDA=0;//STOP:when CLK is high DATA change form low to high
+	CT_Delay();
+	CT_IIC_SDA=1;//发送I2C总线结束信号  
+}
+//等待应答信号到来
+//返回值：1，接收应答失败
+//        0，接收应答成功
+u8 CT_IIC_Wait_Ack(void)
+{
+	u8 ucErrTime=0;
+	CT_SDA_IN();      //SDA设置为输入  
+	CT_IIC_SDA=1;	   
+	CT_IIC_SCL=1;
+	CT_Delay();
+	while(CT_READ_SDA)
+	{
+		ucErrTime++;
+		if(ucErrTime>250)
+		{
+			CT_IIC_Stop();
+			return 1;
+		} 
+		CT_Delay();
+	}
+	CT_IIC_SCL=0;//时钟输出0 	   
+	return 0;  
+} 
+//产生ACK应答
+void CT_IIC_Ack(void)
+{
+	CT_IIC_SCL=0;
+	CT_SDA_OUT();
+	CT_Delay();
+	CT_IIC_SDA=0;
+	CT_Delay();
+	CT_IIC_SCL=1;
+	CT_Delay();
+	CT_IIC_SCL=0;
+}
 
 
