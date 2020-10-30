3f5557b196231b7af35e2f9c4fc2c224435b97ec
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-30 09:16:39 +0800

    Update ft5206.c

---
+++ HARDWARE/TOUCH/ft5206.c
@@ -0,0 +1,23 @@
+#include "ft5206.h"
+#include "touch.h"
+#include "ctiic.h"
+#include "usart.h"
+#include "delay.h" 
+#include "string.h" 
+//////////////////////////////////////////////////////////////////////////////////	 
+//ALIENTEK STM32开发板
+//7寸电容触摸屏-FT5206 驱动代码	   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//All rights reserved									  
+////////////////////////////////////////////////////////////////////////////////// 
+ 
+
+
+
+
+
+
+
+
+
