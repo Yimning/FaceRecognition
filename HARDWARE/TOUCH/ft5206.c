47f9c56638ad8757dc51f3273fb65eb74fcf1d79
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-30 09:18:49 +0800

    Update ft5206.c

--- HARDWARE/TOUCH/ft5206.c
+++ HARDWARE/TOUCH/ft5206.c
@@ -37,5 +37,70 @@ u8 FT5206_WR_Reg(u16 reg,u8 *buf,u8 len)
 }
 //从FT5206读出一次数据
 //reg:起始寄存器地址
+//buf:数据缓缓存区
+//len:读数据长度			  
+void FT5206_RD_Reg(u16 reg,u8 *buf,u8 len)
+{
+	u8 i; 
+ 	CT_IIC_Start();	
+ 	CT_IIC_Send_Byte(FT_CMD_WR);   	//发送写命令 	 
+	CT_IIC_Wait_Ack(); 	 										  		   
+ 	CT_IIC_Send_Byte(reg&0XFF);   	//发送低8位地址
+	CT_IIC_Wait_Ack();  
+ 	CT_IIC_Start();  	 	   
+	CT_IIC_Send_Byte(FT_CMD_RD);   	//发送读命令		   
+	CT_IIC_Wait_Ack();	   
+	for(i=0;i<len;i++)
+	{	   
+    	buf[i]=CT_IIC_Read_Byte(i==(len-1)?0:1); //发数据	  
+	} 
+    CT_IIC_Stop();//产生一个停止条件     
+} 
+//初始化FT5206触摸屏
+//返回值:0,初始化成功;1,初始化失败 
+u8 FT5206_Init(void)
+{
+	u8 temp[2]; 
+	RCC->AHB1ENR|=1<<1;    		//使能PORTB时钟 
+	RCC->AHB1ENR|=1<<2;    		//使能PORTC时钟  
+	GPIO_Set(GPIOB,PIN1,GPIO_MODE_IN,0,0,GPIO_PUPD_PU); 	//PB1设置为上拉输入
+	GPIO_Set(GPIOC,PIN13,GPIO_MODE_OUT,GPIO_OTYPE_PP,GPIO_SPEED_100M,GPIO_PUPD_PU); //PC13设置为推挽输出
+	CT_IIC_Init();      	//初始化电容屏的I2C总线  
+	FT_RST=0;				//复位
+	delay_ms(20);
+ 	FT_RST=1;				//释放复位		    
+	delay_ms(50);  	
+	temp[0]=0;
+	FT5206_WR_Reg(FT_DEVIDE_MODE,temp,1);	//进入正常操作模式 
+	FT5206_WR_Reg(FT_ID_G_MODE,temp,1);		//查询模式 
+	temp[0]=22;								//触摸有效值，22，越小越灵敏	
+	FT5206_WR_Reg(FT_ID_G_THGROUP,temp,1);	//设置触摸有效值
+	temp[0]=12;								//激活周期，不能小于12，最大14
+	FT5206_WR_Reg(FT_ID_G_PERIODACTIVE,temp,1); 
+	//读取版本号，参考值：0x3003
+	FT5206_RD_Reg(FT_ID_G_LIB_VERSION,&temp[0],2);  
+	if(temp[0]==0X30&&temp[1]==0X03)//版本:0X3003
+	{
+		printf("CTP ID:%x\r\n",((u16)temp[0]<<8)+temp[1]);
+		return 0;
+	} 
+	return 1;
+}
+const u16 FT5206_TPX_TBL[5]={FT_TP1_REG,FT_TP2_REG,FT_TP3_REG,FT_TP4_REG,FT_TP5_REG};
+//扫描触摸屏(采用查询方式)
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
 
 
