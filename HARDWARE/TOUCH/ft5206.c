77df81ec3c5443a62e2c808438ff90a9973189c6
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-30 09:18:25 +0800

    Update ft5206.c

--- HARDWARE/TOUCH/ft5206.c
+++ HARDWARE/TOUCH/ft5206.c
@@ -17,6 +17,25 @@
 //buf:数据缓缓存区
 //len:写数据长度
 //返回值:0,成功;1,失败.
-
+u8 FT5206_WR_Reg(u16 reg,u8 *buf,u8 len)
+{
+	u8 i;
+	u8 ret=0;
+	CT_IIC_Start();	 
+	CT_IIC_Send_Byte(FT_CMD_WR);	//发送写命令 	 
+	CT_IIC_Wait_Ack(); 	 										  		   
+	CT_IIC_Send_Byte(reg&0XFF);   	//发送低8位地址
+	CT_IIC_Wait_Ack();  
+	for(i=0;i<len;i++)
+	{	   
+    	CT_IIC_Send_Byte(buf[i]);  	//发数据
+		ret=CT_IIC_Wait_Ack();
+		if(ret)break;  
+	}
+    CT_IIC_Stop();					//产生一个停止条件	    
+	return ret; 
+}
+//从FT5206读出一次数据
+//reg:起始寄存器地址
 
 
