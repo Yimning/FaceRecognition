c878f7a8f7eb8439014697211491596286bd4d75
Yimning <58767468+Yimning@users.noreply.github.com>
2020-10-31 18:33:32 +0800

    Update gt9147.c

--- HARDWARE/TOUCH/gt9147.c
+++ HARDWARE/TOUCH/gt9147.c
@@ -19,10 +19,67 @@
 //GT9147配置参数表
 //第一个字节为版本号(0X60),必须保证新的版本号大于等于GT9147内部
 //flash原有版本号,才会更新配置.
-
-
-
-
+const u8 GT9147_CFG_TBL[]=
+{ 
+	0X60,0XE0,0X01,0X20,0X03,0X05,0X35,0X00,0X02,0X08,
+	0X1E,0X08,0X50,0X3C,0X0F,0X05,0X00,0X00,0XFF,0X67,
+	0X50,0X00,0X00,0X18,0X1A,0X1E,0X14,0X89,0X28,0X0A,
+	0X30,0X2E,0XBB,0X0A,0X03,0X00,0X00,0X02,0X33,0X1D,
+	0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X32,0X00,0X00,
+	0X2A,0X1C,0X5A,0X94,0XC5,0X02,0X07,0X00,0X00,0X00,
+	0XB5,0X1F,0X00,0X90,0X28,0X00,0X77,0X32,0X00,0X62,
+	0X3F,0X00,0X52,0X50,0X00,0X52,0X00,0X00,0X00,0X00,
+	0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,
+	0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X0F,
+	0X0F,0X03,0X06,0X10,0X42,0XF8,0X0F,0X14,0X00,0X00,
+	0X00,0X00,0X1A,0X18,0X16,0X14,0X12,0X10,0X0E,0X0C,
+	0X0A,0X08,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,
+	0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,
+	0X00,0X00,0X29,0X28,0X24,0X22,0X20,0X1F,0X1E,0X1D,
+	0X0E,0X0C,0X0A,0X08,0X06,0X05,0X04,0X02,0X00,0XFF,
+	0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,
+	0X00,0XFF,0XFF,0XFF,0XFF,0XFF,0XFF,0XFF,0XFF,0XFF,
+	0XFF,0XFF,0XFF,0XFF,
+};  
+//发送GT9147配置参数
+//mode:0,参数不保存到flash
+//     1,参数保存到flash
+u8 GT9147_Send_Cfg(u8 mode)
+{
+	u8 buf[2];
+	u8 i=0;
+	buf[0]=0;
+	buf[1]=mode;	//是否写入到GT9147 FLASH?  即是否掉电保存
+	for(i=0;i<sizeof(GT9147_CFG_TBL);i++)buf[0]+=GT9147_CFG_TBL[i];//计算校验和
+    buf[0]=(~buf[0])+1;
+	GT9147_WR_Reg(GT_CFGS_REG,(u8*)GT9147_CFG_TBL,sizeof(GT9147_CFG_TBL));//发送寄存器配置
+	GT9147_WR_Reg(GT_CHECK_REG,buf,2);//写入校验和,和配置更新标记
+	return 0;
+} 
+//向GT9147写入一次数据
+//reg:起始寄存器地址
+//buf:数据缓缓存区
+//len:写数据长度
+//返回值:0,成功;1,失败.
+u8 GT9147_WR_Reg(u16 reg,u8 *buf,u8 len)
+{
+	u8 i;
+	u8 ret=0;
+	CT_IIC_Start();	
+ 	CT_IIC_Send_Byte(GT_CMD_WR);   	//发送写命令 	 
+	CT_IIC_Wait_Ack();
+	CT_IIC_Send_Byte(reg>>8);   	//发送高8位地址
+	CT_IIC_Wait_Ack(); 	 										  		   
+	CT_IIC_Send_Byte(reg&0XFF);   	//发送低8位地址
+	CT_IIC_Wait_Ack();  
+	for(i=0;i<len;i++)
+	{	   
+    	CT_IIC_Send_Byte(buf[i]);  	//发数据
+		ret=CT_IIC_Wait_Ack();
+		if(ret)break;  
+	}
+    CT_IIC_Stop();					//产生一个停止条件	    
+	return ret; 
 
 
 
