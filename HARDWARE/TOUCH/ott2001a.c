c9a1829c31774346ade606fa559f5b0c1a859f43
Yimning <58767468+Yimning@users.noreply.github.com>
2020-11-01 12:11:28 +0800

    Update ott2001a.c

--- HARDWARE/TOUCH/ott2001a.c
+++ HARDWARE/TOUCH/ott2001a.c
@@ -1,2 +1,13 @@
+#include "ott2001a.h"
+#include "touch.h"
+#include "ctiic.h"
+#include "usart.h"
+#include "delay.h" 
+
+//ALIENTEK STM32F407开发板
+//4.3寸电容触摸屏-OTT2001A 驱动代码	   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+
 
 
