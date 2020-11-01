30e1100080dee364dba453543899828b2dddbd1c
Yimning <58767468+Yimning@users.noreply.github.com>
2020-11-01 12:11:49 +0800

    Update ott2001a.c

--- HARDWARE/TOUCH/ott2001a.c
+++ HARDWARE/TOUCH/ott2001a.c
@@ -10,4 +10,33 @@
 //技术论坛:www.openedv.com
 
 
+//向OTT2001A写入一次数据
+//reg:起始寄存器地址
+//buf:数据缓缓存区
+//len:写数据长度
+//返回值:0,成功;1,失败.
+u8 OTT2001A_WR_Reg(u16 reg,u8 *buf,u8 len)
+{
+	u8 i;
+	u8 ret=0;
+	CT_IIC_Start();	
+ 	CT_IIC_Send_Byte(OTT_CMD_WR);   //发送写命令 	 
+	CT_IIC_Wait_Ack();
+	CT_IIC_Send_Byte(reg>>8);   	//发送高8位地址
+	CT_IIC_Wait_Ack(); 	 										  		   
+	CT_IIC_Send_Byte(reg&0XFF);   	//发送低8位地址
+	CT_IIC_Wait_Ack();  
+	for(i=0;i<len;i++)
+	{	   
+    	CT_IIC_Send_Byte(buf[i]);  	//发数据
+		ret=CT_IIC_Wait_Ack();
+		if(ret)break;  
+	}
+    CT_IIC_Stop();					//产生一个停止条件	    
+	return ret; 
+}
+
+
+
+
 
