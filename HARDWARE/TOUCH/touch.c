0bca76c96e0b6da271345c8fa49a11d06af02275
Yimning <58767468+Yimning@users.noreply.github.com>
2020-11-02 09:31:39 +0800

    Update touch.c

--- HARDWARE/TOUCH/touch.c
+++ HARDWARE/TOUCH/touch.c
@@ -81,6 +81,160 @@ u16 TP_Read_AD(u8 CMD)
 	TCS=1;		//释放片选	 
 	return(Num);   
 }
+//读取一个坐标值(x或者y)
+//连续读取READ_TIMES次数据,对这些数据升序排列,
+//然后去掉最低和最高LOST_VAL个数,取平均值 
+//xy:指令（CMD_RDX/CMD_RDY）
+//返回值:读到的数据
+#define READ_TIMES 5 	//读取次数
+#define LOST_VAL 1	  	//丢弃值
+u16 TP_Read_XOY(u8 xy)
+{
+	u16 i, j;
+	u16 buf[READ_TIMES];
+	u16 sum=0;
+	u16 temp;
+	for(i=0;i<READ_TIMES;i++)buf[i]=TP_Read_AD(xy);		 		    
+	for(i=0;i<READ_TIMES-1; i++)//排序
+	{
+		for(j=i+1;j<READ_TIMES;j++)
+		{
+			if(buf[i]>buf[j])//升序排列
+			{
+				temp=buf[i];
+				buf[i]=buf[j];
+				buf[j]=temp;
+			}
+		}
+	}	  
+	sum=0;
+	for(i=LOST_VAL;i<READ_TIMES-LOST_VAL;i++)sum+=buf[i];
+	temp=sum/(READ_TIMES-2*LOST_VAL);
+	return temp;   
+} 
+//读取x,y坐标
+//最小值不能少于100.
+//x,y:读取到的坐标值
+//返回值:0,失败;1,成功。
+u8 TP_Read_XY(u16 *x,u16 *y)
+{
+	u16 xtemp,ytemp;			 	 		  
+	xtemp=TP_Read_XOY(CMD_RDX);
+	ytemp=TP_Read_XOY(CMD_RDY);	  												   
+	//if(xtemp<100||ytemp<100)return 0;//读数失败
+	*x=xtemp;
+	*y=ytemp;
+	return 1;//读数成功
+}
+//连续2次读取触摸屏IC,且这两次的偏差不能超过
+//ERR_RANGE,满足条件,则认为读数正确,否则读数错误.	   
+//该函数能大大提高准确度
+//x,y:读取到的坐标值
+//返回值:0,失败;1,成功。
+#define ERR_RANGE 50 //误差范围 
+u8 TP_Read_XY2(u16 *x,u16 *y) 
+{
+	u16 x1,y1;
+ 	u16 x2,y2;
+ 	u8 flag;    
+    flag=TP_Read_XY(&x1,&y1);   
+    if(flag==0)return(0);
+    flag=TP_Read_XY(&x2,&y2);	   
+    if(flag==0)return(0);   
+    if(((x2<=x1&&x1<x2+ERR_RANGE)||(x1<=x2&&x2<x1+ERR_RANGE))//前后两次采样在+-50内
+    &&((y2<=y1&&y1<y2+ERR_RANGE)||(y1<=y2&&y2<y1+ERR_RANGE)))
+    {
+        *x=(x1+x2)/2;
+        *y=(y1+y2)/2;
+        return 1;
+    }else return 0;	  
+}  
+//////////////////////////////////////////////////////////////////////////////////		  
+//与LCD部分有关的函数  
+//画一个触摸点
+//用来校准用的
+//x,y:坐标
+//color:颜色
+void TP_Drow_Touch_Point(u16 x,u16 y,u16 color)
+{
+	POINT_COLOR=color;
+	LCD_DrawLine(x-12,y,x+13,y);//横线
+	LCD_DrawLine(x,y-12,x,y+13);//竖线
+	LCD_DrawPoint(x+1,y+1);
+	LCD_DrawPoint(x-1,y+1);
+	LCD_DrawPoint(x+1,y-1);
+	LCD_DrawPoint(x-1,y-1);
+	LCD_Draw_Circle(x,y,6);//画中心圈
+}	  
+//画一个大点(2*2的点)		   
+//x,y:坐标
+//color:颜色
+void TP_Draw_Big_Point(u16 x,u16 y,u16 color)
+{	    
+	POINT_COLOR=color;
+	LCD_DrawPoint(x,y);//中心点 
+	LCD_DrawPoint(x+1,y);
+	LCD_DrawPoint(x,y+1);
+	LCD_DrawPoint(x+1,y+1);	 	  	
+}						  
+//////////////////////////////////////////////////////////////////////////////////		  
+//触摸按键扫描
+//tp:0,屏幕坐标;1,物理坐标(校准等特殊场合用)
+//返回值:当前触屏状态.
+//0,触屏无触摸;1,触屏有触摸
+u8 TP_Scan(u8 tp)
+{			   
+	if(PEN==0)//有按键按下
+	{
+		if(tp)TP_Read_XY2(&tp_dev.x[0],&tp_dev.y[0]);//读取物理坐标
+		else if(TP_Read_XY2(&tp_dev.x[0],&tp_dev.y[0]))//读取屏幕坐标
+		{
+	 		tp_dev.x[0]=tp_dev.xfac*tp_dev.x[0]+tp_dev.xoff;//将结果转换为屏幕坐标
+			tp_dev.y[0]=tp_dev.yfac*tp_dev.y[0]+tp_dev.yoff;  
+	 	} 
+		if((tp_dev.sta&TP_PRES_DOWN)==0)//之前没有被按下
+		{		 
+			tp_dev.sta=TP_PRES_DOWN|TP_CATH_PRES;//按键按下  
+			tp_dev.x[4]=tp_dev.x[0];//记录第一次按下时的坐标
+			tp_dev.y[4]=tp_dev.y[0];  	   			 
+		}			   
+	}else
+	{
+		if(tp_dev.sta&TP_PRES_DOWN)//之前是被按下的
+		{
+			tp_dev.sta&=~(1<<7);//标记按键松开	
+		}else//之前就没有被按下
+		{
+			tp_dev.x[4]=0;
+			tp_dev.y[4]=0;
+			tp_dev.x[0]=0xffff;
+			tp_dev.y[0]=0xffff;
+		}	    
+	}
+	return tp_dev.sta&TP_PRES_DOWN;//返回当前的触屏状态
+}	  
+//////////////////////////////////////////////////////////////////////////	 
+//保存在EEPROM里面的地址区间基址,占用13个字节(RANGE:SAVE_ADDR_BASE~SAVE_ADDR_BASE+12)
+#define SAVE_ADDR_BASE 40
+//保存校准参数										    
+void TP_Save_Adjdata(void)
+{
+	s32 temp;			 
+	//保存校正结果!		   							  
+	temp=tp_dev.xfac*100000000;//保存x校正因素      
+    AT24CXX_WriteLenByte(SAVE_ADDR_BASE,temp,4);   
+	temp=tp_dev.yfac*100000000;//保存y校正因素    
+    AT24CXX_WriteLenByte(SAVE_ADDR_BASE+4,temp,4);
+	//保存x偏移量
+    AT24CXX_WriteLenByte(SAVE_ADDR_BASE+8,tp_dev.xoff,2);		    
+	//保存y偏移量
+	AT24CXX_WriteLenByte(SAVE_ADDR_BASE+10,tp_dev.yoff,2);	
+	//保存触屏类型
+	AT24CXX_WriteOneByte(SAVE_ADDR_BASE+12,tp_dev.touchtype);	
+	temp=0X0A;//标记校准过了
+	AT24CXX_WriteOneByte(SAVE_ADDR_BASE+13,temp); 
+}
+
 
 
 
