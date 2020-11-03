764a1b6debe0046fa83c61735de1f16a96358db1
Yimning <58767468+Yimning@users.noreply.github.com>
2020-11-03 10:21:48 +0800

    Update tpad.c

---
+++ HARDWARE/TPAD/tpad.c
@@ -0,0 +1,10 @@
+#include "tpad.h"
+#include "delay.h"		    
+#include "usart.h"
+
+//ALIENTEK STM32F407开发板
+//TPAD驱动代码	   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//All rights reserved									  
+////////////////////////////////////////////////////////////////////////////////// 	
