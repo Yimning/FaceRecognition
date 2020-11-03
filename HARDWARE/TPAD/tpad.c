ad17f5bff5bafc5ceb26a0ce7627f58620ee2f9e
Yimning <58767468+Yimning@users.noreply.github.com>
2020-11-03 10:24:56 +0800

    Update tpad.c

--- HARDWARE/TPAD/tpad.c
+++ HARDWARE/TPAD/tpad.c
@@ -84,8 +84,32 @@ u16 TPAD_Get_MaxVal(u8 n)
 	};
 	return res;
 }  
-
-
+//扫描触摸按键
+//mode:0,不支持连续触发(按下一次必须松开才能按下一次);1,支持连续触发(可以一直按下)
+//返回值:0,没有按下;1,有按下;										  
+#define TPAD_GATE_VAL 	100	//触摸的门限值,也就是必须大于tpad_default_val+TPAD_GATE_VAL,才认为是有效触摸.
+u8 TPAD_Scan(u8 mode)
+{
+	static u8 keyen=0;	//0,可以开始检测;>0,还不能开始检测	 
+	u8 res=0;
+	u8 sample=3;	//默认采样次数为3次	 
+	u16 rval;
+	if(mode)
+	{
+		sample=6;	//支持连按的时候，设置采样次数为6次
+		keyen=0;	//支持连按	  
+	}
+	rval=TPAD_Get_MaxVal(sample); 
+	if(rval>(tpad_default_val+TPAD_GATE_VAL)&&rval<(10*tpad_default_val))//大于tpad_default_val+TPAD_GATE_VAL,且小于10倍tpad_default_val,则有效
+	{							 
+		if(keyen==0)res=1;	//keyen==0,有效 
+		//printf("r:%d\r\n",rval);		     	    					   
+		keyen=3;				//至少要再过3次之后才能按键有效   
+	} 
+	if(keyen)keyen--;		   							   		     	    					   
+	return res;
+}	 
+//定时器2通道2输入捕获配置	 
 
 
 
