88f2d979dc88f6bbaf0e6f08a36a7237d5910ffd
Yimning <58767468+Yimning@users.noreply.github.com>
2020-11-05 10:16:29 +0800

    Update w25qxx.c

--- HARDWARE/W25QXX/w25qxx.c
+++ HARDWARE/W25QXX/w25qxx.c
@@ -221,7 +221,24 @@ void W25QXX_Erase_Chip(void)
 	W25QXX_Wait_Busy();   				   //等待芯片擦除结束
 }   
 //擦除一个扇区
-
+//Dst_Addr:扇区地址 根据实际容量设置
+//擦除一个山区的最少时间:150ms
+void W25QXX_Erase_Sector(u32 Dst_Addr)   
+{  
+	//监视falsh擦除情况,测试用   
+ 	printf("fe:%x\r\n",Dst_Addr);	  
+ 	Dst_Addr*=4096;
+    W25QXX_Write_Enable();                  //SET WEL 	 
+    W25QXX_Wait_Busy();   
+  	W25QXX_CS=0;                            //使能器件   
+    SPI1_ReadWriteByte(W25X_SectorErase);      //发送扇区擦除指令 
+    SPI1_ReadWriteByte((u8)((Dst_Addr)>>16));  //发送24bit地址    
+    SPI1_ReadWriteByte((u8)((Dst_Addr)>>8));   
+    SPI1_ReadWriteByte((u8)Dst_Addr);  
+	W25QXX_CS=1;                            //取消片选     	      
+    W25QXX_Wait_Busy();   				   //等待擦除完成
+}  
+//等待空闲
 
 
 
