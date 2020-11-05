427ce7495eadc556cbac0510f4fe5d2a474a014d
Yimning <58767468+Yimning@users.noreply.github.com>
2020-11-05 10:13:52 +0800

    Update w25qxx.c

--- HARDWARE/W25QXX/w25qxx.c
+++ HARDWARE/W25QXX/w25qxx.c
@@ -1,44 +1,12 @@
+#include "w25qxx.h" 
+#include "spi.h"
+#include "delay.h"	   
+#include "usart.h"	
 
+//ALIENTEK STM32F407开发板
+//W25QXX 驱动代码	   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
+u16 W25QXX_TYPE=W25Q128;	//默认是W25Q128
