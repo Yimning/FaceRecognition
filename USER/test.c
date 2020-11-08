6710b351c12bf1776241daa48ab86f0305940397
Yimning <58767468+Yimning@users.noreply.github.com>
2020-11-08 09:43:16 +0800

    Update test.c

--- USER/test.c
+++ USER/test.c
@@ -29,18 +29,27 @@
 //广州市星翼电子科技有限公司
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+int reg_time;
+u8 ov2640_mode=0;						//工作模式:0,RGB565模式;1,JPEG模式
+ 
+ 
+//处理JPEG数据
+//当采集完一帧JPEG数据后,调用此函数,切换JPEG BUF.开始下一帧采集.
+void jpeg_data_process(void)
+{ 
+	if(ov2640_mode)//只有在JPEG格式下,才需要做处理.
+	{ 
+	}
+} 
+//切换为OV2640模式
+void sw_ov2640_mode(void)
+{
+	OV2640_PWDN=0;//OV2640 Power Up
+	//GPIOC8/9/11切换为 DCMI接口
+ 	GPIO_AF_Set(GPIOC,8,13);	//PC8,AF13  DCMI_D2
+ 	GPIO_AF_Set(GPIOC,9,13);	//PC9,AF13  DCMI_D3
+ 	GPIO_AF_Set(GPIOC,11,13);	//PC11,AF13 DCMI_D4  
+} 
 
 
 
