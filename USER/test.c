53c964d2ae7e11ba6122fb6cbefab7a09a19ddd2
Yimning <58767468+Yimning@users.noreply.github.com>
2020-11-08 09:42:40 +0800

    Update test.c

--- USER/test.c
+++ USER/test.c
@@ -1,3 +1,49 @@
+#include "sys.h"
+#include "delay.h"  
+#include "usart.h"   
+#include "led.h"
+#include "lcd.h"
+#include "key.h"  
+#include "usmart.h"   
+#include "sram.h" 
+#include "malloc.h" 
+#include "w25qxx.h"    
+#include "sdio_sdcard.h"
+#include "ff.h"  
+#include "exfuns.h"    
+#include "fontupd.h"
+#include "text.h"	
+#include "piclib.h"	
+#include "string.h"	
+#include "math.h"	
+#include "dcmi.h"	
+#include "ov2640.h"	
+#include "beep.h"	
+#include "timer.h"
+
+#include "atk_frec.h"
+ 
+//ALIENTEK 探索者STM32F407开发板 实验x
+//人脸识别 测试 实验 
+//技术支持：www.openedv.com
+//广州市星翼电子科技有限公司
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
